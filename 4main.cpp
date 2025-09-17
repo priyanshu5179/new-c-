9724890bbec4adf375bfb38ffbdc9dd9322f8335
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-09-18 00:12:34 +0530

    Create 4main.cpp
    finding lowercase and uppercase

---
+++ 4main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char ch ;
+    cout << "enter ch: ";
+    cin >> ch;
+
+    if (ch>=a && ch<=z) {
+        cout << "lowercase" << endl;
+    } else {
+        cout << "uppercase" << endl;
+    }
+
+    return 0;
+}
