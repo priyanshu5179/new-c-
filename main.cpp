55666f3e0032f2d26fcbbae9710bf21e92df7e03
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-09-17 22:31:03 +0530

    Update main.cpp
    add new code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+///#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6;
+    //int sum = a + b;
+    int multiply = (a*b);
+    //cout << "The sum of two values is: " << sum << endl;
+    cout << "multiplication of two number:" << multiply << endl;
+    return 0;
+}///
 #include <iostream>
 using namespace std;
 
